c7c4175c881716571e99aa5a3677ec96d6b37f52
marcoscosta90 <marcoscosta.dev@gmail.com>
2021-03-29 16:36:20 -0300

    barber project with react native

---
+++ app/src/screens/Preload/index.js
@@ -0,0 +1,32 @@
+/* eslint-disable prettier/prettier */
+import React, { useEffect } from 'react';
+import { Container, LoadingIcon } from './styles';
+import AsyncStorage from '@react-native-community/async-storage';
+import { useNavigation } from '@react-navigation/native';
+
+import BarberLogo from '../../assets/barber.svg';
+
+export default () => {
+
+    const navigation = useNavigation();
+
+
+    useEffect(() => {
+        const checkToken = async () => {
+            const token = await AsyncStorage.getItem('token');
+            if(token !== null) {
+                //validar o token
+            } else {
+                navigation.navigate('SignIn');
+            }
+        }
+        checkToken();
+    }, []);
+
+    return (
+        <Container>
+            <BarberLogo width="100%" height="160" />
+            <LoadingIcon size="large" color="#ffffff" />
+        </Container>
+    );
+}
